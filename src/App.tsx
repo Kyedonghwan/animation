f893f4d854749d23f312aa00d8287ea499eb8657
Kyedonghwan <kdh6437@nts-corp.com>
2022-06-30 22:32:35 +0900

    shared layout by layoutId

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,29 @@ import { motion, useMotionValue, useTransform, useViewportScroll, AnimatePresenc
 import { useRef, useState } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
-  width: 100vw;
   display: flex;
-  justify-content: center;
+  height: 100vh;
+  justify-content: space-around;
+  align-items: center;
   background: linear-gradient(135deg,rgb(238, 0, 153), rgb(221, 0, 238))
 `
 
 const Box = styled(motion.div)`
-  width: 200px;
   height: 200px;
   background-color: white;
   border-radius: 40px;
   display: flex;
-  justify-content: center;
-  align-items: center;
   box-shadow: 0 2px 3px rgba(0,0,0,0.1), 0 10px 20px rgba(0,0,0,0.06);
-  font-size: 18px;
-  font-weight: 700;
-  position: absolute;
-  top: 100px;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child, div:last-child {
+    grid-column: span 2;
+  }
 `
 
 const Svg = styled(motion.svg)`
@@ -34,70 +37,37 @@ const Svg = styled(motion.svg)`
   }
 `
 
-const ButtonWrap = styled.div`
-  display: flex;
-  height: 30px;
-  margin-top: 350px;
-  
-  button {
-    &:last-child {
-      margin-left: 30px;
-    }
-
-    border-radius: 5px;
-    background-color: white;
-    border: 0;
-  }
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
 `
 
-const boxVariant = {
-  invisible: (back:boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0
-  }),
-  visible: (back: boolean) => ({
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1
-    }
-  }),
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 1
-    }
-  }),
-}
-
-
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,0.5);
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 function App() {
 
-  const [showing, setShowing] = useState(1);
-  const [back, setBack] = useState(false);
-
-  const nextPlease = () => {setShowing(prev=>prev===10? 10: prev+1);
-    setBack(false);
-  };
-
-  const prevPlease = () => {setShowing(prev=>prev===1? 1 : prev-1);
-    setBack(true);
-  }
+  const [id, setId] = useState<null | string>(null);
 
   return (
     <>
       <Wrapper>
-        <AnimatePresence custom={back}>
-          <Box custom={back} variants={boxVariant} initial="invisible" exit="exit" animate="visible" key={showing}>{showing}</Box>
-          <ButtonWrap>
-            <button onClick={prevPlease}>Prev</button> 
-            <button onClick={nextPlease}>Next</button>
-          </ButtonWrap>
+        <Grid>
+          {[1, 2, 3, 4].map( n => <Box onClick={() => setId(n+"")} layoutId={n+""} />)}
+        </Grid>
+        <AnimatePresence>
+          {id ? <Overlay onClick={() => setId(null)} initial={{opacity: 0}} animate={{opacity: 1 }} exit={{opacity: 0}}>
+            <Box layoutId={id} style={{width: 400, height: 200}} />
+          </Overlay> : null}
         </AnimatePresence>
       </Wrapper>
     </>
